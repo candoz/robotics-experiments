8faa09ed5f58a179dbd380cad35d58e53b894404
Marco Canducci <marco.candoz@gmail.com>
2019-04-02 12:21:34 +0200

    Implement seeing_some_light function

--- lab02/beginning.lua
+++ lab02/beginning.lua
@@ -3,30 +3,48 @@ MOVE_STEPS = 10
 n_steps = 0
 
 function init()
-    n_steps = 0
-	robot.leds.set_all_colors("black")
+  n_steps = 0
+  robot.leds.set_all_colors("black")
 end
 
 function step()
-    if n_steps % MOVE_STEPS == 0 then
-        robot.wheels.set_velocity(wander())
-    end
-    n_steps = n_steps + 1
+  if n_steps % MOVE_STEPS == 0 then
+    follow_the_light()
+  end
+  n_steps = n_steps + 1
 end
 
 function reset()
-	robot.wheels.set_velocity(0,0)
-	n_steps = 0
-	robot.leds.set_all_colors("black")
+  robot.wheels.set_velocity(0,0)
+  n_steps = 0
+  robot.leds.set_all_colors("black")
 end
 
 --[[ This function is executed only once, when the robot is removed from the simulation ]]
 function destroy()
-   -- put your code here
+  -- put your code here
 end
+
 
 -- Behaviours:
 
 function wander()
-    return robot.random.uniform(10), robot.random.uniform(10)
+  robot.wheels.set_velocity(robot.random.uniform(10), robot.random.uniform(10))
+end
+
+function follow_the_light()
+  if not seeing_some_light() then
+    wander()
+  else
+    log("I see the light!")
+  end
+end
+
+-- Utils:
+
+function seeing_some_light()
+  for _, light_sensor in pairs(robot.light) do
+    if light_sensor.value > 0 then return true end
+  end
+  return false
 end
