6b7ad80fcfe6dc04ae84e23fd0c3cd13e488a25b
Marco Canducci <marco.candoz@gmail.com>
2019-04-16 20:23:36 +0200

    Remove useless utility function. Pass from many proximity sensors to just the higher one

--- lab02/code/arbitration-subsumption.lua
+++ lab02/code/arbitration-subsumption.lua
@@ -1,8 +1,10 @@
 MAX_WHEEL_SPEED = 10
 SLOW_WHEEL_SPEED = 2
 
-PROXIMITY_THRESHOLD = 0.05
+PROXIMITY_THRESHOLD = 0.02
 SATISFIED_LIGHT_VALUE = 0.35 -- change this value accordingly to the light height
+
+highest_light_sensor = nil
 
 function init()
   robot.leds.set_all_colors("black")
@@ -10,6 +12,8 @@ function init()
 end
 
 function step()
+  highest_light_sensor = get_sensor_with_highest_value(robot.light)
+  highest_proximity_sensor = get_sensor_with_highest_value(robot.proximity)
   stand_still() -- this naming upsets me deeply ...
 end
 
@@ -28,22 +32,40 @@ function go_straight()
 end
 
 function follow_the_light() -- otherwise go straight
-  if not seeing_some_light() then
+  if highest_light_sensor.value == 0 then
     go_straight()
   else
-    light_direction = get_sensor_with_highest_value(robot.light).angle
-    if between(light_direction, math.pi/2, math.pi) then sign_l = -1 else sign_l = 1 end
-    if between(light_direction, -math.pi, -math.pi/2) then sign_r = -1 else sign_r = 1 end
-    mod_l = 1 - math.sin(light_direction)
-    mod_r = 1 + math.sin(light_direction)
-
-    log("Mods:" .. mod_l .. "|" .. mod_r)
-
+    if between(highest_light_sensor.angle, math.pi/2, math.pi) then sign_l = -1 else sign_l = 1 end
+    if between(highest_light_sensor.angle, -math.pi, -math.pi/2) then sign_r = -1 else sign_r = 1 end
+    mod_l = 1 - math.sin(highest_light_sensor.angle)
+    mod_r = 1 + math.sin(highest_light_sensor.angle)
     k_norm = MAX_WHEEL_SPEED / math.max(mod_l, mod_r)
     speed_l = sign_l * mod_l * k_norm
     speed_r = sign_r * mod_r * k_norm
     robot.wheels.set_velocity(speed_l, speed_r)
-    log("Following the light, wheel speeds -> L:" .. math.floor(speed_l*100)/100 .. " R:" .. math.floor(speed_r*100)/100)
+    log("Following the light -> L:" .. math.floor(speed_l*100)/100 .. " R:" .. math.floor(speed_r*100)/100)
+  end
+end
+
+function coast_border() -- otherwise follow the light
+  if highest_proximity_sensor.value > PROXIMITY_THRESHOLD
+      and between(highest_proximity_sensor.angle, math.pi/2, math.pi*7/8)
+      and (highest_light_sensor.value == 0 or between(highest_light_sensor.angle, 0, math.pi)) then
+    l = 1 + math.cos(highest_proximity_sensor.angle)
+    r = 1 - math.cos(highest_proximity_sensor.angle)
+    k_norm = MAX_WHEEL_SPEED / math.max(l, r)
+    robot.wheels.set_velocity(l * k_norm, r * k_norm)
+    log("Coasting a wall on the LEFT -> L:" .. math.floor(l*k_norm*100)/100 .. " R:" .. math.floor(r*k_norm*100)/100)
+  elseif highest_proximity_sensor.value > PROXIMITY_THRESHOLD
+      and between(highest_proximity_sensor.angle, -math.pi*7/8, -math.pi/2)
+      and (highest_light_sensor.value == 0 or between(highest_light_sensor.angle, -math.pi, 0)) then
+    l = 1 - math.cos(highest_proximity_sensor.angle)
+    r = 1 + math.cos(highest_proximity_sensor.angle)
+    k_norm = MAX_WHEEL_SPEED / math.max(l, r)
+    robot.wheels.set_velocity(l * k_norm, r * k_norm)
+    log("Coasting a wall on the LEFT -> L:" .. math.floor(l*k_norm*100)/100 .. " R:" .. math.floor(r*k_norm*100)/100)
+  else
+    follow_the_light()
   end
 end
 
@@ -63,10 +85,10 @@ function avoid_crash() -- otherwise follow the light
   end
 end
 
-function stand_still()
-  if get_sensor_with_highest_value(robot.light).value >= SATISFIED_LIGHT_VALUE then
+function stand_still() -- otherwise avoid crash
+  if highest_light_sensor.value >= SATISFIED_LIGHT_VALUE then
     robot.wheels.set_velocity(0, 0)
-    log("Arrived :)")
+    log("Arrived to destination :)")
   else
     avoid_crash()
   end
@@ -82,46 +104,11 @@ end
 
 -- Returns the highest value given a map of sensors, where every sensor has a "value" field
 function get_sensor_with_highest_value(sensors)
-  highest = null
+  highest = nil
   for _, sensor in pairs(sensors) do
-    if highest == null or highest.value < sensor.value then 
+    if highest == nil or highest.value < sensor.value then 
       highest = sensor
     end
   end
   return highest
-end
-
--- Returns true if the robot has at least a light sensor value higher than the threshold
-function seeing_some_light()
-  for _, sensor in pairs(robot.light) do
-    if sensor.value > 0 then return true end
-  end
-  return false
-end
-
--- Returns true if at least one proximity sensor 
-function sensing_obstacles_ahead(threshold)
-  for _, sensor in pairs(robot.proximity) do
-    if sensor.value > threshold and between(sensor.angle, -math.pi/2, math.pi/2) then 
-      return true 
-    end
-  end
-  return false
-end
-
---[[ Returns a couple of values (left, right) with the total proximities values for
-the sensors positioned top-left and top-right respectively ]]
-function get_proximities_left_right()
-  left = 0
-  right = 0
-  for _, proximity_sensor in pairs(robot.proximity) do
-    value = proximity_sensor.value
-    angle = proximity_sensor.angle
-    if between(angle, 0, math.pi/2) then
-      left = left + value
-    elseif between(angle, -math.pi/2, 0) then
-      right = right + value
-    end
-  end
-  return left, right
 end
